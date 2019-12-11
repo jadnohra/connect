ac9d7ba5d087e751deab4c206d98f68cc3cd257c
Jad Nohra <jadnohra@gmail.com>
2019-12-11 18:33:53 +0100

    More yaml testing (trying a simple query)

--- main_yaml.py
+++ main_yaml.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python3
 
+import argparse
 import yaml
 import pprint
+from collections.abc import Iterable
 
 pp = pprint.PrettyPrinter(indent=4)
 
-db = yaml.load(open('./data/esu-ma-735.yaml'))
+parser = argparse.ArgumentParser(description='Query the connect .yaml file')
+parser.add_argument('--file', help='The connect .yaml file')
+parser.add_argument("query", help="The search query [key]:[value]", default=None)
+args = parser.parse_args()
+
+db = yaml.load(open(args.file if args.file else './data/esu-ma-735.yaml'), Loader=yaml.FullLoader)
 pp.pprint(db)
+
+def recurse_gather(node, key, value, path=''):
+    gathered = []
+    for key, item in node.items():
+        if isinstance(item, dict):
+            gathered.extend(recurse_gather(item, key, value, '{}.{}'.format(path, key)))
+        else:
+            if (key == '' or node in key) or \
+                (value == '' or item in value):
+                gathered.append('{}.{}:{}'.format(path, key, value))
+    return gathered
+
+query = args.query
+key,value = query.split(':')
+
+print(key, value)
+pp.pprint(recurse_gather(db, key, value))
