77eb94ae99c5b81006c89a8758cfc74ff08ddabe
Jad Nohra <jadnohra@gmail.com>
2020-03-22 13:32:20 +0100

    more fol lang

--- core/lang/fol_replacement.py
+++ core/lang/fol_replacement.py
@@ -2,7 +2,6 @@
 References:
  - Symbolic Logic, Copi, p.33, 396
 '''
-from typing import List
 from .fol_lang import Wff, PropVarWff, BinaryWff, PropositionalVariable, NegWff
 
 
@@ -144,3 +143,96 @@ class DoubleNegation(Replacement):
 
     def short_name(self):
         return 'DN'
+
+
+class Transposition(Replacement):
+    def __init__(self):
+        p = PropVarWff(PropositionalVariable('p'))
+        q = PropVarWff(PropositionalVariable('q'))
+        pat1 = BinaryWff.new_impl(p, q)
+        pat2 = BinaryWff.new_impl(NegWff(q), NegWff(p))
+        super().__init__(pat1, pat2)
+
+    def short_name(self):
+        return 'TRANS'
+
+
+class MaterialImplication(Replacement):
+    def __init__(self):
+        p = PropVarWff(PropositionalVariable('p'))
+        q = PropVarWff(PropositionalVariable('q'))
+        pat1 = BinaryWff.new_impl(p, q)
+        pat2 = BinaryWff.new_disj(NegWff(p), q)
+        super().__init__(pat1, pat2)
+
+    def short_name(self):
+        return 'IMPL'
+
+
+class MaterialEquivalenceConj(Replacement):
+    def __init__(self):
+        p = PropVarWff(PropositionalVariable('p'))
+        q = PropVarWff(PropositionalVariable('q'))
+        pat1 = BinaryWff.new_equiv(p, q)
+        pat2 = BinaryWff.new_conj(
+                BinaryWff.new_impl(p, q),
+                BinaryWff.new_impl(q, p)
+                )
+        super().__init__(pat1, pat2)
+
+    def short_name(self):
+        return 'EQUIVc'
+
+
+class MaterialEquivalenceDisj(Replacement):
+    def __init__(self):
+        p = PropVarWff(PropositionalVariable('p'))
+        q = PropVarWff(PropositionalVariable('q'))
+        pat1 = BinaryWff.new_equiv(p, q)
+        pat2 = BinaryWff.new_disj(
+                BinaryWff.new_conj(p, q),
+                BinaryWff.new_conj(NegWff(q), NegWff(q))
+                )
+        super().__init__(pat1, pat2)
+
+    def short_name(self):
+        return 'EQUIVd'
+
+
+class Exportation(Replacement):
+    def __init__(self):
+        p = PropVarWff(PropositionalVariable('p'))
+        q = PropVarWff(PropositionalVariable('q'))
+        r = PropVarWff(PropositionalVariable('r'))
+        pat1 = BinaryWff.new_impl(
+                BinaryWff.new_conj(p, q),
+                r)
+        pat2 = BinaryWff.new_impl(
+                p,
+                BinaryWff.new_impl(q, r))
+        super().__init__(pat1, pat2)
+
+    def short_name(self):
+        return 'EXP'
+
+
+class TautologyConj(Replacement):
+    def __init__(self):
+        p = PropVarWff(PropositionalVariable('p'))
+        pat1 = p
+        pat2 = BinaryWff.new_conj(p, p)
+        super().__init__(pat1, pat2)
+
+    def short_name(self):
+        return 'TAUTc'
+
+
+class TautologyDisj(Replacement):
+    def __init__(self):
+        p = PropVarWff(PropositionalVariable('p'))
+        pat1 = p
+        pat2 = BinaryWff.new_conj(p, p)
+        super().__init__(pat1, pat2)
+
+    def short_name(self):
+        return 'TAUTd'
