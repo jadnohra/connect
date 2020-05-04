2622b4b1e712c38aafc667e8c3cc3b0bf3b6a11a
Jad Nohra <jadnohra@gmail.com>
2020-05-04 19:00:20 +0200

    Adding FOL and ST topic

--- ddq/fol/formula.py
+++ ddq/fol/formula.py
@@ -4,5 +4,3 @@ from .thing import Thing
 class Formula(Thing):
     def __init__(self):
         super().__init__(self)
-
-    
--- ddq/fol/quantifier.py
+++ ddq/fol/quantifier.py
@@ -1,16 +1,43 @@
+from ddq.node import Node
+
+
 class BindContext:
-    def __init__(self, parent: "BindContext" = None):
-        self._parent = parent
+    def __init__(self):
+        self._id_counter = 0
 
     def new_variable(self) -> "BoundVariable":
-        pass
+        yield BoundVariable(self._id_counter)
+        self._id_counter = self._id_counter + 1
 
 
 class BoundVariable:
-    def __init__(self):
+    def __init__(self, id: int):
+        self._id = id
+
+
+class Quantifier(Node):
+    def __init__(self, bind_context: BindContext = None):
+        self._bind_context = (BindContext()
+                              if bind_context is None
+                              else bind_context)
+        self._variable = self._bind_context.new_variable()
+
+    def variable(self) -> BoundVariable:
+        return self._variable
+
+    def bind_context(self) -> BindContext:
+        return self._bind_context
+
+    ]def children(self) -> List:
         pass
 
 
-class UniversalQuantifier(BindContext):
-    def __init__(self, parent_context: BindContext = None):
-        super().__init__(parent_context)
+class UniversalQuantifier(Quantifier):
+    def __init__(self, bind_context: BindContext = None):
+        super().__init__(bind_context)
+
+
+class ExistentialQuantifier(Quantifier):
+    def __init__(self, bind_context: BindContext = None):
+        super().__init__(bind_context)
+
